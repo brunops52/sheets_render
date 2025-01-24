44b6e0d417c384c7b9f575fe39c9112540d939d5
brunops52 <139251367+brunops52@users.noreply.github.com>
2025-01-24 13:48:32 +0000

    feat: Get list sheets

--- sheets_render/src/main.js
+++ sheets_render/src/main.js
@@ -2,22 +2,25 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+const apiUrl = 'https://sheetdb.io/api/v1/tno9krv8jdrnp';
+  
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
 
+    const contentDiv = document.getElementById('api-content');
+    
+    if (data) {
+      contentDiv.innerHTML = `
+        <pre>${JSON.stringify(data, null, 2)}</pre>
+      `;
+    } else {
+      contentDiv.innerHTML = '<p>TESTE_FALHA</p>';
+    }
+  });
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+  <div id="api-content">
+
   </div>
 `
 
